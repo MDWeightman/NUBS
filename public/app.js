d4b298a991820d2fb0c509c342fb4d6adfa18616
Micah Weightman <micah.weightman@hillarys.co.uk>
2017-04-12 11:51:20 +0100

    _User class added

--- public/app.js
+++ public/app.js
@@ -10,34 +10,64 @@ var config = {
 firebase.initializeApp(config);
 
 
+class _User {
+	constructor() {
+		this.id = null;
+		this.data = null;
+		this.accessToken = null;
+	}
+
+	setId(id){
+		this.id = id;
+	}
+	setData(data){
+		this.data = data;
+	}
+
+	getFb() {
+		FB.api('/me', {fields: 'id,name,birthday,gender,religion,picture,email', access_token: App.user.accessToken}, function(data) {
+			console.log(data);
+			App.user.update(data);
+		});
+	}
+	getDb(id) {
+		App.user.setId(id);
+		App.database.ref(`user/${App.user.id}`).on('value', function(snapshot) {
+			App.user.setData(snapshot.val());
+			App.user.draw();
+		});
+	}
+
+	update(fbUser){
+		if(fbUser.picture && fbUser.picture.data && fbUser.picture.data.url){
+			fbUser.picture = fbUser.picture.data.url;
+		}
+		var user = App.database.ref(`user/${App.user.id}`).set(fbUser);
+		console.log(user);
+	}
+
+	draw(){		
+		var HTML = `
+		<ul class="collection">
+			<li class="collection-item avatar ${this.data.gender}">
+				<img src="${this.data.picture}" alt="" class="circle">
+				<span class="title">${this.data.name}</span>
+				<p>
+					${this.data.birthday}
+					<br/>
+					${this.data.religion}
+				</p>
+			</li>
+		</ul>`
+		document.getElementById("container").innerHTML = HTML;
+	}
+}
+
 class _App {
 
 	constructor() {
 		this.database = firebase.database();
-		this.userId = null;
-		this.user = null;
-		this.accessToken = null;
-	}
-
-	getFBUser() {
-		FB.api('/me', {fields: 'id,name,birthday,gender,religion,picture,email', access_token: App.accessToken}, function(data) {
-			console.log(data);
-			App.updateUser(data);
-		});
-	}
-	getUser() {
-		this.database.ref(`user/${this.userId}`).on('value', function(snapshot) {
-			App.user = snapshot.val();
-			App.drawUser();
-		});
-	}
-
-	updateUser(fbUser){
-		if(fbUser.picture && fbUser.picture.data && fbUser.picture.data.url){
-			fbUser.picture = fbUser.picture.data.url;
-		}
-		var user = this.database.ref(`user/${this.userId}`).set(fbUser);
-		console.log(user);
+		this.user = new _User();
 	}
 
 	login() {
@@ -45,35 +75,13 @@ class _App {
 		provider.addScope('user_birthday');
 		firebase.auth().signInWithPopup(provider).then(function(result) {
 			App.accessToken = result.credential.accessToken;
-			App.userId = result.user.uid;
-			App.displayName = result.user.displayName;
-			App.getUser();
+			App.user.getDb(result.user.uid);
 		}).catch(function(error) {
 			var errorCode = error.code;
 			var errorMessage = error.message;
 			var email = error.email;
 			var credential = error.credential;
 		});
-	}
-
-	drawUser(){		
-		var HTML = `
-		<ul class="collection">
-			<li class="collection-item avatar ${this.user.gender}">
-				<img src="${this.user.picture}" alt="" class="circle">
-				<span class="title">${this.user.name}</span>
-				<p>
-					${this.user.birthday}
-					<br/>
-					${this.user.religion}
-				</p>
-			</li>
-		</ul>`
-		document.getElementById("container").innerHTML = HTML;
-	}
-
-	sendInvitation(userId){
-
 	}
 
 }
